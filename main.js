750a47fe8658629dac7640f34942e1e3c8c8ece5
Jacob Britton <jacobbritton95@gmail.com>
2022-06-27 08:07:05 -0400

    Implement main resizing function of button

--- main.js
+++ main.js
@@ -1,25 +1,65 @@
-setGrid(10);
+btnClick();
+
+
+function btnClick() {
+    const btn = document.querySelector('#btn')
+    btn.addEventListener('click', () => btnPopup(btn));
+}
+
+
+function btnPopup (element) {
+    while(1) {
+        let enteredGridSize = prompt("Enter grid dimension (1-100)", "");
+
+        if (enteredGridSize === null) {
+            return;
+        }
+        else if (
+            Number(enteredGridSize) >= 1 &&
+            Number(enteredGridSize) <= 100
+        ) {
+            setGrid(Number(enteredGridSize));
+            return;
+        } else {
+            alert("Invalied grid dimension (must be between 1 and 100)");
+        }
+    }
+}
+
 
 function setGrid(gridSize) {
-    let cellSize = 500 / gridSize;     // in px
-    let divContainer = document.querySelector('#container');
+    let cellSize = 500 / gridSize;
+    const divContainer = document.querySelector('#container');
+    clearContainer(divContainer);
 
     for (let i = 0; i < gridSize**2 ; i++) {
-        let cell = document.createElement('div');
+        const cell = document.createElement('div');
         setAttributes(cell, cellSize);
         addHighlighting(cell);
         divContainer.appendChild(cell);
     }
 }
 
+
+function clearContainer(element) {
+    if (element.firstChild){
+        while(element.hasChildNodes()) {
+            element.removeChild(element.lastChild)
+        }
+    }
+
+}
+
+
 function setAttributes(element, gridSize) {
     element.setAttribute('style', 
-    `border: 1px solid black;
-     box-sizing: border-box;
-     flex-shrink: 0;
-     height: ${gridSize};     
-     width: ${gridSize}px;`);
+        `border: 1px solid black;
+         box-sizing: border-box;
+         flex-shrink: 0;
+         height: ${gridSize};     
+         width: ${gridSize}px;`);
 }
+
 
 function addHighlighting(element) {
     element.addEventListener('mouseover', () => {
